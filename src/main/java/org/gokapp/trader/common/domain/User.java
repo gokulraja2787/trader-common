de10dd252e59af5482ee436ceb22f5e38528ba68
Gokul RANGARAJAN <gokulraja2006@mail.com>
2020-12-12 17:36:31 +0530

    Added user object

---
+++ src/main/java/org/gokapp/trader/common/domain/User.java
@@ -0,0 +1,63 @@
+package org.gokapp.trader.common.domain;
+
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@RedisHash("user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    @Getter
+    @Setter
+    private String userId;
+
+    @Getter
+    @Setter
+    private String userName;
+
+    @Getter
+    @Setter
+    private Set<String> groups;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (userId == null) {
+            if (other.userId != null)
+                return false;
+        } else if (!userId.equals(other.userId))
+            return false;
+        if (userName == null) {
+            if (other.userName != null)
+                return false;
+        } else if (!userName.equals(other.userName))
+            return false;
+        return true;
+    }
+
+    
+}
